41b241438124c36d1253704041e8190c37949c6b
srushti.sraybhoge <srushti.sraybhoge@gmail.com>
2025-12-30 17:41:36 +0000

    creds removed

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import { getAuth } from "firebase/auth";
 
 // TODO: Replace with your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC3GTn3_oowpt2GRnkwtN6g0DiRdgs1gEo",
-  authDomain: "ticked-off-ui.firebaseapp.com",
-  projectId: "ticked-off-ui",
-  storageBucket: "ticked-off-ui.firebasestorage.app",
-  messagingSenderId: "746599190136",
-  appId: "1:746599190136:web:9a71c0f533d010f33fa78d"
+  
 };
 
 // Initialize Firebase
